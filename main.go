a3b45f60a49b2e08600746d7219298cbcaf3be6d
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2026-09-12 01:37:37 +0300

    Add CLI interface

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/5c077m4n/tmux-state/state"
+	"github.com/5c077m4n/tmux-state/cli"
 )
 
 func main() {
-	s, err := state.GetTmuxState()
-	if err != nil {
+	if err := cli.Execute(); err != nil {
 		panic(err)
 	}
-	stateJSON, _ := json.MarshalIndent(s, "", "\t")
-	fmt.Printf("tmux state: %+v\n", string(stateJSON))
 }
